06a250d0a9785645802242201532411f3e3e22a9
Zonxv2 <matias1garcia2@gmail.com>
2026-02-27 01:48:28 -0300

    Delete 06 - TESTING directory

--- 06 - TESTING/test-integrales/01-supertest/src/middlewares/error-handler.js
+++
@@ -1,8 +0,0 @@
-import { CustomError } from "../utils/custom-error.js";
-import { httpResponse } from "../utils/http-response.js";
-
-export const errorHandler = (error, req, res, next) => {
-  console.log(error);
-  if (error instanceof CustomError) return httpResponse.CustomError(res, error);
-  return httpResponse.ServerError(res, error);
-};
