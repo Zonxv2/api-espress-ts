bbbaa14fdba2e4501e30398184a284b3c49930cc
Zonxv2 <matias1garcia2@gmail.com>
2026-02-27 01:48:59 -0300

    Delete 02 - TESTING Y OPTIMIZACION directory

--- 02 - TESTING Y OPTIMIZACION/02-mocking/src/routes/product-router.js
+++
@@ -1,9 +0,0 @@
-import * as Controllers from "../controllers/product-controllers.js";
-import { Router } from "express";
-
-const router = Router();
-
-router.get("/", Controllers.getProducts);
-router.post("/", Controllers.createProductMock);
-
-export default router;
